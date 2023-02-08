c1058736f1020f4b7267241387c6129b3da4b0c3
szabikr <szabikr.dev@gmail.com>
2023-02-08 12:20:03 +0200

    Matrix transformations

---
+++ matrix_transformations.py
@@ -0,0 +1,28 @@
+def print_matrix(matrix):
+    for i in range(0, len(matrix)):
+        for j in range(0, len(matrix[0])):
+            print(matrix[i][j], end=', ')
+        print('')
+
+def rotate_matrix(matrix):
+    m = []
+    for j in range(0, len(matrix[0])):
+        row = []
+        for i in range(0, len(matrix)):
+            row.append(matrix[i][j])
+        m.append(row)
+    return m
+
+t = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16],
+]
+
+if __name__ == "__main__":
+    print('Initial:')
+    print_matrix(t)
+    m = rotate_matrix(t)
+    print('Rotated:')
+    print_matrix(m)

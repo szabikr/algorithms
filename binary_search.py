6e5e95c7bcdeb0c97477235569bc5c36a9d17ec4
szabikr <szabikr.dev@gmail.com>
2023-03-17 06:36:05 +0200

    Implements binary search

---
+++ binary_search.py
@@ -0,0 +1,18 @@
+def binary_search(array, item):
+    low = 0
+    high = len(array) - 1
+
+    while low <= high:
+        pos = (low + high) // 2
+        guess = array[pos]
+        if guess == item:
+            return pos
+        if guess > item:
+            high = pos - 1
+        else:
+            low = pos + 1
+    return None
+
+if __name__ == "__main__":
+    array = [0, 12, 14, 15, 19, 20, 27, 28, 33, 35, 37, 38, 44, 61, 65, 67, 69, 82, 96, 99]
+    print(binary_search(array, 44))

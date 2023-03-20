820b674eb74a38dfac298175f464327c222a90f6
szabikr <szabikr.dev@gmail.com>
2023-03-20 10:44:51 +0200

    Removes empty line

--- list/binary_search.py
+++ list/binary_search.py
@@ -7,7 +7,6 @@ from typing import Optional
 def binary_search(array: list[int], item: int) -> Optional[int]:
     low = 0
     high = len(array) - 1
-
     while low <= high:
         pos = (low + high) // 2
         guess = array[pos]

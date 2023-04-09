c82ba269c8782ca7de49f63d2bd7e6fabff4dd38
szabikr <szabikr.dev@gmail.com>
2023-04-09 07:55:22 +0300

    Improves selection sort

--- list/selection_sort.py
+++ list/selection_sort.py
@@ -1,25 +1,29 @@
 """
+Alogrihm complexity: O(n^2)
 Sort a list of elements using the Selection Sort technique.
 
 Find the index of the smallest element and move it to the new list.
 """
 
 def find_min(l: list[int]) -> int:
-    min = l[0]
-    min_index = 0
+    min, min_index = l[0], 0
     for i in range(1, len(l)):
         if l[i] < min:
-            min = l[i]
-            min_index = i
+            min, min_index = l[i], i
     return min_index
 
 def selection_sort(l: list[int]) -> list[int]:
     result = []
-    while l:
-        result.append(l.pop(find_min(l)))
+    for _ in range(len(l)):
+        min = find_min(l)
+        result.append(l.pop(min))
     return result
+
+def selection_sort_2(l: list[int]) -> list[int]:
+    return [l.pop(find_min(l)) 
+            for _ in range(len(l))]
 
 if __name__ == "__main__":
     l = [56, 65, 74, 100, 99, 68, 86, 180, 90]
     print(l)
-    print(selection_sort(l))
+    print(selection_sort_2(l))
